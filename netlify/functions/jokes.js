9ac3d23b531e336ea0f83b39e4f56b5acbda23ae
smtdfc <smtdfc.master@gmail.com>
2025-04-16 22:11:52 +0700

    update

--- netlify/functions/jokes.js
+++ netlify/functions/jokes.js
@@ -2,26 +2,41 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async function(event, context) {
-
   const filePath = path.join(__dirname, 'jokes.json');
   
-  try {
 
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      },
+      body: '',
+    };
+  }
+  
+  try {
     const data = fs.readFileSync(filePath, 'utf8');
     const jokes = JSON.parse(data).jokes;
     
-
     const randomId = Math.floor(Math.random() * jokes.length);
     const randomJoke = jokes[randomId];
     
-
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', 
+      },
       body: JSON.stringify(randomJoke),
     };
   } catch (error) {
     return {
       statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
       body: JSON.stringify({ message: 'Error reading file or parsing JSON', error: error.message }),
     };
   }
