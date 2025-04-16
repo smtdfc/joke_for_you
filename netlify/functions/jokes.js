6bc1b32fdfda0d54642f532ea8e55ebdd47539f6
smtdfc <smtdfc.master@gmail.com>
2025-04-16 22:17:49 +0700

    update

--- netlify/functions/jokes.js
+++ netlify/functions/jokes.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async function(event, context) {
-  const filePath = path.join(__dirname, 'jokes.json');
-  
 
   if (event.httpMethod === 'OPTIONS') {
     return {
@@ -18,16 +16,18 @@ exports.handler = async function(event, context) {
   }
   
   try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    const jokes = JSON.parse(data).jokes;
+  const filePath = path.join(__dirname, 'jokes.json');
     
-    const randomId = Math.floor(Math.random() * jokes.length);
-    const randomJoke = jokes[randomId];
+   const raw = fs.readFileSync(filePath, 'utf8');
+    const jokes = JSON.parse(raw).jokes;
+    
+    const randomIndex = Math.floor(Math.random() * jokes.length);
+    const randomJoke = jokes[randomIndex];
     
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*', 
+        'Access-Control-Allow-Origin': '*',
       },
       body: JSON.stringify(randomJoke),
     };
@@ -37,7 +37,10 @@ exports.handler = async function(event, context) {
       headers: {
         'Access-Control-Allow-Origin': '*',
       },
-      body: JSON.stringify({ message: 'Error reading file or parsing JSON', error: error.message }),
+      body: JSON.stringify({
+        message: 'Error reading file or parsing JSON',
+        error: error.message,
+      }),
     };
   }
 };
